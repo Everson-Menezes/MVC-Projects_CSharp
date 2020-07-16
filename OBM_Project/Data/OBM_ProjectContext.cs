2f7f7d58f7cdae63fccecd6cefef93d8ff240ae8
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-07-16 10:09:00 -0300

    CRUD Scaffolding

---
+++ OBM_Project/Data/OBM_ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OBM_Project.Models;
+
+namespace OBM_Project.Data
+{
+    public class OBM_ProjectContext : DbContext
+    {
+        public OBM_ProjectContext (DbContextOptions<OBM_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OBM_Project.Models.Departamento> Departamento { get; set; }
+    }
+}
--- OBM_Project/Startup.cs
+++ OBM_Project/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using OBM_Project.Data;
 
 namespace OBM_Project
 {
@@ -33,6 +35,9 @@ namespace OBM_Project
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<OBM_ProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("OBM_ProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
