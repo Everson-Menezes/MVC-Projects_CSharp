cd05d23bfd19ca3137352502e6e19da180e00d04
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-07-29 08:34:18 -0300

    Controller Orcamento criada

---
+++ OBM_Project/Controllers/Orcamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OBM_Project.Controllers
+{
+    public class Orcamento : Controller
+    {
+        public IActionResult Orcar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GerarOrcamento()
+        {
+            ViewBag.Messages = "Seu orçamento ficara pronto em até 60 minutos!";
+            return View("Orcar");
+        }
+    }
+}
