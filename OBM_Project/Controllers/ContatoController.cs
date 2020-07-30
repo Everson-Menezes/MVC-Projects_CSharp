33a015cf5c06568b9e1b861368e4895e91bf14fe
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-07-30 16:54:43 -0300

    Implementando envio de contato 50 porcento

---
+++ OBM_Project/Controllers/ContatoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OBM_Project.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EnviarEmail()
+        {
+            return Content("E-mail enviado com sucesso.");
+        }
+        public IActionResult EnviarWhatsApp()
+        {
+            return Redirect("https://wa.me/5513996664409");
+        }
+    }
+}
