66ccef3970f78b6144ada1fc93946274e4561769
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-07-31 17:01:05 -0300

    Add Painel de controle

---
+++ OBM_Project/Controllers/PainelControleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OBM_Project.Controllers
+{
+    public class PainelControleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdicionarTipoServico()
+        {
+            return View();
+        }
+        public IActionResult AdicionarSubTipoServico()
+        {
+            return View();
+        }
+        public IActionResult AdicionarNecessidade()
+        {
+            return View();
+        }
+    }
+}
