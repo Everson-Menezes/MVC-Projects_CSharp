4511ad1db48ff43f109018c80e76ee3b89f6efa6
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-08-12 15:29:03 -0300

    Atualização login

--- OBM_Project/Controllers/LoginController.cs
+++ OBM_Project/Controllers/LoginController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OBM_Project.Models.Usuario;
+using OBM_Project.Services;
 
 namespace OBM_Project.Controllers
 {
     public class LoginController : Controller
-    {   
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult RedefinirSenha()
         {
             return View();
